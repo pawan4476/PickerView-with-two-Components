c6269028af39d0631bcd954daf49f6c13c17fe57
chaitanya <chaitanya@chaitanyas-mac.local>
2016-08-30 08:53:53 -0700

    Initial Commit

---
+++ test3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test3
+//
+//  Created by chaitanya on 30/08/16.
+//  Copyright © 2016 chaitanya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test3
+//
+//  Created by chaitanya on 30/08/16.
+//  Copyright © 2016 chaitanya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
